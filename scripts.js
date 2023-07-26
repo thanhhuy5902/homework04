dbae2213ddef9e02083748c868836fc314893557
thanhhuy5902 <tranthanhhuy050902@gmail.com>
2023-07-26 16:31:35 +0700

    Home work 04 - vẽ hình nhân vật và animation1

--- scripts.js
+++ scripts.js
@@ -11,7 +11,7 @@ let width = c.width;
 let eyes1 = {
   currentX: 550,
   currentY: 280,
-  targetX: 280,
+  targetX: 550,
   targetY: 280,
 };
 
@@ -20,7 +20,7 @@ let eyes1 = {
 let eyes2 = {
   currentX: 360,
   currentY: 280,
-  targetX: 275,
+  targetX: 360,
   targetY: 220,
 };
 
@@ -34,6 +34,37 @@ document.addEventListener("mousemove", (e2) => {
   eyes2.targetX = e2.clientX;
   eyes2.targetY = e2.clientY;
   console.log(eyes2);
+});
+
+let shellY = 579;
+// let shellsmall = 550;
+// let shellline = 520;
+
+let isEventHandled = false;
+let heart = 519; // Tọa độ y của hình trái tim
+// Tọa độ y mục tiêu của hình trái tim
+
+document.addEventListener("click", (e) => {
+  if (!isEventHandled) {
+    isEventHandled = true;
+    heart.targetX1 = e.clientX; // Lấy tọa độ x của sự kiện click
+    heart.targetY1 = e.clientY;
+    setInterval(() => {
+      // trái tim
+      ctx.beginPath();
+      ctx.moveTo(456, 519);
+      ctx.bezierCurveTo(456, 516, 451, 504, 431, 504);
+      ctx.bezierCurveTo(401, 504, 401, 541.5, 401, 541.5);
+      ctx.bezierCurveTo(401, 559.5, 421, 581.5, 456, 599.5);
+      ctx.bezierCurveTo(491, 581.5, 511, 559.5, 511, 541.5);
+      ctx.bezierCurveTo(511, 541.5, 511, 504, 481, 504);
+      ctx.bezierCurveTo(466, 504, 456, 516, 456, 519);
+      ctx.fillStyle = "#FF0000";
+      ctx.fill();
+      ctx.closePath();
+    });
+    draw();
+  }
 });
 
 let fps = 30;
@@ -92,7 +123,7 @@ function draw() {
 
     ctx.beginPath();
     // ctx.scale(1, 1);
-    ctx.arc(360, 280, 8, 0, Math.PI * 2);
+    ctx.arc(eyes2.currentX, eyes2.currentY, 8, 0, Math.PI * 2);
     ctx.fillStyle = "white";
     ctx.fill();
     ctx.stroke();
@@ -258,9 +289,17 @@ function draw() {
     ctx.stroke();
     ctx.closePath();
 
-    // thân phải
+    // bán nguyệt phải
     ctx.beginPath();
-    ctx.arc(508, 450, 30, 0, Math.PI, true);
+    ctx.arc(543, 435, 15, 0, Math.PI * 2, true);
+    ctx.fillStyle = "#648C9C";
+    ctx.fill();
+    ctx.stroke();
+    ctx.closePath();
+
+    // bán nguyệt trái
+    ctx.beginPath();
+    ctx.arc(363, 437, 15, 0, Math.PI * 2, true);
     ctx.fillStyle = "#648C9C";
     ctx.fill();
     ctx.stroke();
@@ -275,6 +314,8 @@ function draw() {
 
     ctx.beginPath();
     ctx.ellipse(454, 580, 150, 160, Math.PI, 0, Math.PI * 2);
+    ctx.fillStyle = "#648C9C";
+    ctx.fill();
     ctx.stroke();
     ctx.closePath();
 
@@ -282,59 +323,116 @@ function draw() {
     ctx.beginPath();
     ctx.moveTo(585, 500);
     ctx.quadraticCurveTo(580, 580, 550, 580);
-    ctx.quadraticCurveTo(560, 340, 550, 580);
-    // ctx.lineTo(700, 530);
-    // ctx.lineTo(600, 530);
-    ctx.stroke();
+    ctx.quadraticCurveTo(550, 390, 589, 520);
+    ctx.fillStyle = "white";
+    ctx.fill();
+    // ctx.stroke();
     ctx.closePath();
 
     // tay trái
 
     ctx.beginPath();
     ctx.moveTo(325, 500);
-    ctx.quadraticCurveTo(337, 580, 370, 580);
-    ctx.quadraticCurveTo(337, 345, 370, 580);
-    // ctx.lineTo(220, 530);
-    // ctx.lineTo(313, 530);
-    ctx.stroke();
+    ctx.quadraticCurveTo(320, 590, 370, 580);
+    ctx.quadraticCurveTo(320, 400, 325, 530);
+    ctx.fillStyle = "white";
+    ctx.fill();
+    // ctx.stroke();
     ctx.closePath();
 
     // vỏ sò
+    if (!isEventHandled) {
+      // Vẽ vỏ sò khi chưa có sự kiện click
+      ctx.beginPath();
+      ctx.ellipse(456, 579, 20, 20, Math.PI, 0, Math.PI, true);
+      ctx.fillStyle = "#CBB15B";
+      ctx.fill();
+      ctx.stroke();
+      ctx.closePath();
 
-    ctx.beginPath();
-    ctx.ellipse(456, 579, 20, 20, Math.PI, 0, Math.PI, true);
-    ctx.fillStyle = "#CBB15B";
-    ctx.fill();
-    ctx.stroke();
-    ctx.closePath();
+      ctx.beginPath();
+      ctx.ellipse(455, 550, 50, 30, Math.PI, 0, 2 * Math.PI);
+      ctx.fillStyle = "#CBB15B";
+      ctx.fill();
+      ctx.stroke();
+      ctx.closePath();
 
-    // ctx.fillStyle = "black";
-    // ctx.fill()
-    ctx.beginPath();
-    ctx.ellipse(455, 550, 50, 30, Math.PI, 0, 2 * Math.PI);
-    ctx.fillStyle = "#CBB15B";
-    ctx.fill();
-    ctx.stroke();
-    ctx.closePath();
+      ctx.beginPath();
+      ctx.moveTo(470, 520);
+      ctx.bezierCurveTo(480, 530, 480, 560, 470, 580);
+      ctx.stroke();
+      ctx.closePath();
 
-    ctx.beginPath();
-    ctx.moveTo(470, 520);
-    ctx.bezierCurveTo(480, 530, 480, 560, 470, 580);
-    ctx.stroke();
-    ctx.closePath();
+      ctx.beginPath();
+      ctx.moveTo(440, 520);
+      ctx.bezierCurveTo(430, 530, 430, 560, 440, 580);
+      ctx.stroke();
+      ctx.closePath();
+    } else {
+      // Vẽ vỏ sò sau khi có sự kiện click
+      if (shellY < 800) {
+        shellY += 1;
+      } else {
+        shellY = 800;
+      }
+      ctx.beginPath();
+      ctx.ellipse(456, shellY, 20, 20, Math.PI, 0, Math.PI, true);
+      ctx.fillStyle = "#CBB15B";
+      ctx.fill();
+      ctx.stroke();
+      ctx.closePath();
 
-    ctx.beginPath();
-    ctx.moveTo(440, 520);
-    ctx.bezierCurveTo(430, 530, 430, 560, 440, 580);
-    ctx.stroke();
-    ctx.closePath();
-    ctx.beginPath();
+      ctx.beginPath();
+      ctx.ellipse(455, shellY - 29, 50, 30, Math.PI, 0, 2 * Math.PI);
+      ctx.fillStyle = "#CBB15B";
+      ctx.fill();
+      ctx.stroke();
+      ctx.closePath();
+
+      ctx.beginPath();
+      ctx.moveTo(470, shellY - 59);
+      ctx.bezierCurveTo(480, shellY - 49, 480, shellY - 19, 470, shellY - 1);
+      ctx.stroke();
+      ctx.closePath();
+
+      ctx.beginPath();
+      ctx.moveTo(440, shellY - 59);
+      ctx.bezierCurveTo(430, shellY - 49, 430, shellY - 19, 440, shellY - 1);
+      ctx.stroke();
+      ctx.closePath();
+
+      // Vẽ hình trái tim
+      ctx.beginPath();
+      ctx.moveTo(456, 519);
+      ctx.bezierCurveTo(456, 516, 451, 504, 431, 504);
+      ctx.bezierCurveTo(401, 504, 401, 541.5, 401, 541.5);
+      ctx.bezierCurveTo(401, 559.5, 421, 581.5, 456, 599.5);
+      ctx.bezierCurveTo(491, 581.5, 511, 559.5, 511, 541.5);
+      ctx.bezierCurveTo(511, 541.5, 511, 504, 481, 504);
+      ctx.bezierCurveTo(466, 504, 456, 516, 456, 519);
+      ctx.fillStyle = "#FF0000";
+      ctx.fill();
+      ctx.closePath();
+    }
 
     // chân phải
     ctx.beginPath();
     ctx.ellipse(590, 745, 20, 60, Math.PI / 1.45, 0, Math.PI * 2);
     ctx.fillStyle = " #535C7A";
     ctx.fill();
+    ctx.stroke();
+    ctx.closePath();
+
+    ctx.beginPath();
+    ctx.moveTo(642, 776);
+    ctx.lineTo(610, 750);
+    ctx.stroke();
+    ctx.closePath();
+
+    ctx.beginPath();
+    ctx.moveTo(630, 781);
+    ctx.lineTo(590, 750);
+    // ctx.lineWidth = 3;
     ctx.stroke();
     ctx.closePath();
 
@@ -345,6 +443,66 @@ function draw() {
     ctx.fill();
     ctx.stroke();
     ctx.closePath();
+
+    ctx.beginPath();
+    ctx.moveTo(279, 779);
+    ctx.lineTo(315, 750);
+    ctx.stroke();
+    ctx.closePath();
+
+    ctx.beginPath();
+    ctx.moveTo(269, 773);
+    ctx.lineTo(295, 750);
+    ctx.stroke();
+    ctx.closePath();
+
+    if (eyes1.currentX < eyes1.targetX) {
+      eyes1.currentX += 1;
+    } else if (eyes1.currentX > eyes1.targetX) {
+      eyes1.currentX -= 1;
+    }
+    if (eyes1.currentX > 565) {
+      eyes1.currentX -= 1;
+    }
+    if (eyes1.currentX < 535) {
+      eyes1.currentX += 1;
+    }
+
+    if (eyes1.currentY < eyes1.targetY) {
+      eyes1.currentY += 1;
+    } else if (eyes1.currentY > eyes1.targetY) {
+      eyes1.currentY -= 1;
+    }
+    if (eyes1.currentY > 295) {
+      eyes1.currentY -= 1;
+    }
+    if (eyes1.currentY < 260) {
+      eyes1.currentY += 1;
+    }
+
+    if (eyes2.currentX < eyes2.targetX) {
+      eyes2.currentX += 1;
+    } else if (eyes2.currentX > eyes2.targetX) {
+      eyes2.currentX -= 1;
+    }
+    if (eyes2.currentX > 375) {
+      eyes2.currentX -= 1;
+    }
+    if (eyes2.currentX < 345) {
+      eyes2.currentX += 1;
+    }
+
+    if (eyes2.currentY < eyes2.targetY) {
+      eyes2.currentY += 1;
+    } else if (eyes2.currentY > eyes2.targetY) {
+      eyes2.currentY -= 1;
+    }
+    if (eyes2.currentY > 295) {
+      eyes2.currentY -= 1;
+    }
+    if (eyes2.currentY < 265) {
+      eyes2.currentY += 1;
+    }
   }, fpsInterval);
 }
 
